c2c04a90ae6e1f226b708bd5f33c45175d83276d
shuvam-sa <shuvam.jyoti-saha@capgemini.com>
2023-04-21 00:07:51 +0530

    first commit

---
+++ Components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {HomeIcon } from '@heroicons/react/24/outline'
+function Sidebar() {
+    return (
+        <div>
+
+        <div>
+            <button>
+                <HomeIcon className="h-6 w-6"/>
+            </button>
+        </div>
+
+        </div>
+    )
+    
+}
+
+export default Sidebar
