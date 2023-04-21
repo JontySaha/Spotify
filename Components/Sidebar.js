f36f3dcc6b3c61f2a979532df72e5ace002518fd
JontySaha <jontysaha600@gmail.com>
2023-04-21 10:02:45 +0530

    Sidebar in progress

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -1,18 +1,40 @@
 import React from "react";
-import {HomeIcon } from '@heroicons/react/24/outline'
+import { HomeIcon,MagnifyingGlassIcon,BuildingLibraryIcon,PlusCircleIcon,HeartIcon,RssIcon } from "@heroicons/react/24/outline";
+
 function Sidebar() {
-    return (
-        <div>
-
-        <div>
-            <button>
-                <HomeIcon className="h-6 w-6"/>
-            </button>
-        </div>
-
-        </div>
-    )
-    
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <MagnifyingGlassIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <BuildingLibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900"></hr>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <p>Your episodes</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900"></hr>
+        <p className="cursor-pointer hover:text-white">Playlist...</p>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
